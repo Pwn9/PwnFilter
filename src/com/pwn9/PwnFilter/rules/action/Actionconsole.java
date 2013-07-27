b236c997e1ad39ea9ea832f2e20aadd74b73bb83
Patrick Toal <ptoal@takeflight.ca>
2013-07-27 00:09:59 -0400

    Fixed bug where console commands are executed from an Async tasks, when they should be run synchronously.

--- src/com/pwn9/PwnFilter/rules/action/Actionconsole.java
+++ src/com/pwn9/PwnFilter/rules/action/Actionconsole.java
@@ -3,6 +3,7 @@ package com.pwn9.PwnFilter.rules.action;
 import com.pwn9.PwnFilter.FilterState;
 import com.pwn9.PwnFilter.util.Patterns;
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 
 /**
  * Execute a console command
@@ -17,9 +18,14 @@ public class Actionconsole implements Action {
     }
 
     public boolean execute(final FilterState state ) {
-        String cmd = Patterns.replaceCommands(command, state);
+        final String cmd = Patterns.replaceCommands(command, state);
         state.addLogMessage("Sending console command: " + cmd);
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd);
+        Bukkit.getScheduler().runTask(state.plugin, new BukkitRunnable() {
+            @Override
+            public void run() {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd);
+            }
+        });
         return true;
     }
 }
