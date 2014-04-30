53679b93e6819ea6c4e2c162e2e31f15e57922c1
Patrick Toal <ptoal@takeflight.ca>
2014-04-29 20:38:29 -0400

    Modify ColoredString so that it allows a configurable format prefix. If format prefix = '\0', ColoredString will behave similarly to String.  Changed replaceText method to not strip formatting preceeding match text.

--- test/com/pwn9/PwnFilter/util/ColoredStringTest.java
+++ test/com/pwn9/PwnFilter/util/ColoredStringTest.java
@@ -23,11 +23,11 @@ public class ColoredStringTest extends TestCase {
 
 
     public void testColoredString() {
-        String testString = "§7This §9is§l the §1string§3 under test.";
+        String testString = "&7This &9is&l the &1string&3 under test.";
         String plainString = "This is the string under test.";
 
-        String[] codeArray = {"§7",null,null,null,null,"§9",null,"§l",
-                null,null,null,null,"§1",null,null,null,null,null,"§3",null,null,
+        String[] codeArray = {"&7",null,null,null,null,"&9",null,"&l",
+                null,null,null,null,"&1",null,null,null,null,null,"&3",null,null,
                 null,null,null,null,null,null,null,null,null,null};
 
         ColoredString cs = new ColoredString(testString);
@@ -49,18 +49,18 @@ public class ColoredStringTest extends TestCase {
         Pattern p = Pattern.compile("test");
         ColoredString modified = cs.replaceText(p, "derp");
         assertEquals("This is the string under derp.",modified.getPlainString());
-        assertEquals("§7This §9is§l the §1string§3 under derp.",modified.getColoredString());
+        assertEquals("&7This &9is&l the &1string&3 under derp.",modified.getColoredString());
 
         // Replace with colored text
         p = Pattern.compile("string");
-        modified = cs.replaceText(p, "§3r§4a§5i§6n§7b§8o§9w");
+        modified = cs.replaceText(p, "&3r&4a&5i&6n&7b&8o&9w");
         assertEquals("This is the rainbow under test.", modified.getPlainString());
-        assertEquals("§7This §9is§l the §1§3r§4a§5i§6n§7b§8o§9w§3 under test.", modified.getColoredString());
+        assertEquals("&7This &9is&l the &1&3r&4a&5i&6n&7b&8o&9w&3 under test.", modified.getColoredString());
 
     }
 
     public void testColoredStringWithDoubleFormatting() {
-        String test = "§9§l![MEMBERS]!:§r §aPlease §c§l/VOTE§r§a to get §9§l4 DIAMONDS§r§a /VOTE!";
+        String test = "&9&l![MEMBERS]!:&r &aPlease &c&l/VOTE&r&a to get &9&l4 DIAMONDS&r&a /VOTE!";
         String plain = "![MEMBERS]!: Please /VOTE to get 4 DIAMONDS /VOTE!";
         Pattern p = Pattern.compile("-+m+e+m+b+e+r");
 
@@ -71,45 +71,54 @@ public class ColoredStringTest extends TestCase {
 
         ColoredString cs2 = new ColoredString("-member");
 
-        ColoredString modified2 = cs2.replaceText(p,"§9§l![MEMBERS]!:§r §aPlease §c§l/VOTE§r§a to get §9§l4 DIAMONDS§r§a /VOTE!");
-        assertEquals("§9§l![MEMBERS]!:§r §aPlease §c§l/VOTE§r§a to get §9§l4 DIAMONDS§r§a /VOTE!",modified2.getColoredString());
+        ColoredString modified2 = cs2.replaceText(p,"&9&l![MEMBERS]!:&r &aPlease &c&l/VOTE&r&a to get &9&l4 DIAMONDS&r&a /VOTE!");
+        assertEquals("&9&l![MEMBERS]!:&r &aPlease &c&l/VOTE&r&a to get &9&l4 DIAMONDS&r&a /VOTE!",modified2.getColoredString());
     }
 
     /**
      * Test that a string that ends with a format code is properly represented
      */
     public void testColoredStringWithFormatAtEnd() {
-        String test = "This is a string ending with format.§3";
+        String test = "This is a string ending with format.&3";
         ColoredString cs = new ColoredString(test);
         assertEquals(cs.getColoredString(),test);
     }
 
+    public void testReplaceText() {
+        // Complex string test.  Make sure that characters are not lost.
+        String test = "a&3test.";
+        ColoredString cs = new ColoredString(test);
+        ColoredString result = cs.replaceText(Pattern.compile("test"), "derp&4");
+        assertEquals(result.getColoredString(),"a&3derp&4.");
+    }
+
     public void testReplaceWithFormatAtEnd() {
-        String test = "Test string.§1";
+        String test = "Test string.&1";
         Pattern p = Pattern.compile("string");
         String replacement = "hello";
 
         ColoredString cs = new ColoredString(test);
-        assertEquals("Test hello.§1",cs.replaceText(p,replacement).getColoredString());
+        assertEquals("Test hello.&1",cs.replaceText(p,replacement).getColoredString());
     }
 
     public void testReplaceWithFormatAtEndOfReplacement() {
         String test = "Test string.";
         Pattern p = Pattern.compile("string");
-        String replacement = "hello§1";
+        String replacement = "hello&1";
 
         ColoredString cs = new ColoredString(test);
-        assertEquals("Test hello§1.", cs.replaceText(p,replacement).getColoredString());
+        assertEquals("Test hello&1.", cs.replaceText(p,replacement).getColoredString());
 
     }
 
     public void testConsecutiveReplacementsWithFormatting() {
-        String test = "This is a §1test§2test§3test.";
+        String test = "&This is a &1test&2testa&3test.&";
         Pattern p = Pattern.compile("test");
-        String replacement = "derp§4";
-
+        String replacement = "derp&4";
         ColoredString cs = new ColoredString(test);
-        assertEquals("This is a §1derp§4§2derp§4§3derp§4.",cs.replaceText(p,replacement).getColoredString());
+        ColoredString result = cs.replaceText(p, replacement);
+        assertEquals("&This is a &1derp&4&2derp&4a&3derp&4.&",result.getColoredString());
     }
+
 
 }
